aab4b55f7410a212b6ed8a8107d4c1b5607f9877
Montassir Fouzi <montifouzi@gmail.com>
2023-04-10 08:06:00 +0200

    Update message.py

--- message.py
+++ message.py
@@ -1,2 +1,11 @@
-print("I love you!")
-input("Press Enter to close...")
+import tkinter as tk
+from tkinter import messagebox
+
+def show_message():
+    messagebox.showinfo("Message", "I love you!")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
+    show_message()
+    root.mainloop()
